6d49987ae42c923701818ac13303e0b9ff170f0f
Abilton Dias <abiltongd@gmail.com>
2019-08-01 21:43:31 -0300

    Projeto Criado

---
+++ src/test/java/com/abiltondias/cursosmc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abiltondias.cursosmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
