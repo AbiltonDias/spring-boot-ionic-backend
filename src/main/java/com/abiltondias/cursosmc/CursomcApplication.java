e11b17c3f5a1dbeb14ae45065188b2ea9784dc42
Abilton Dias <abiltongd@gmail.com>
2019-08-13 22:31:17 -0300

    Criando operacao de instaciacao

--- src/main/java/com/abiltondias/cursosmc/CursomcApplication.java
+++ src/main/java/com/abiltondias/cursosmc/CursomcApplication.java
@@ -1,14 +1,33 @@
 package com.abiltondias.cursosmc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.abiltondias.cursosmc.domain.Categoria;
+import com.abiltondias.cursosmc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 		
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria c1 = new Categoria(null, "Informatica");
+		Categoria c2 = new Categoria(null, " Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(c1,c2));
 		
 	}
 
